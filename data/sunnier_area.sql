a441c7e67cc7bd35cb2a434f7709985ee0129fcb
Brian Mount <bvmount@gmail.com>
2013-10-23 03:00:06 -0700

    Check in grids in pg dump format.

---
+++ data/sunnier_area.sql
@@ -0,0 +1,39 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: sunnier_area; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE sunnier_area (
+    geog geography
+);
+
+
+--
+-- Data for Name: sunnier_area; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY sunnier_area (geog) FROM stdin;
+0103000020E6100000010000000C000000FFFFFF3F35995EC0245567A891E54240000000C0EE9B5EC0FBACE690B8E2424000000070D59B5EC0C30A67A48EE14240FFFFFF3F329C5EC06B9FC2983EE1424001000050139C5EC0861D2604C9DF4240000000505F9B5EC0C7F64223BEDE424000000000659A5EC043EE3D8D9ADE42400000001057985EC018849F4E4FDC4240FFFFFF9FB9965EC038CA43B62BDC4240FFFFFF4FCD965EC05BE0BE08C7DE4240010000100B995EC00F26113565E54240FFFFFF3F35995EC0245567A891E54240
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
